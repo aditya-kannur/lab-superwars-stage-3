12f37fc7e75309b38c55c478a978ad5a896f4f33
AdityaKannur <adityakannur28@gmail.com>
2024-03-10 00:01:51 +0530

    completed superwars-stage-3

--- src/app.js
+++ src/app.js
@@ -1,33 +1,37 @@
 const PLAYERS = [
-    "Spiderman",
-    "Captain America",
-    "Wonderwoman",
-    "Popcorn",
-    "Gemwoman",
-    "Bolt",
-    "Antwoman",
-    "Mask",
-    "Tiger",
-    "Captain",
-    "Catwoman",
-    "Fish",
-    "Hulk",
-    "Ninja",
-    "Black Cat",
-    "Volverine",
-    "Thor",
-    "Slayer",
-    "Vader",
-    "Slingo"
+    'Spiderman',
+    'Captain America',
+    'Wonderwoman',
+    'Popcorn',
+    'Gemwoman',
+    'Bolt',
+    'Antwoman',
+    'Mask',
+    'Tiger',
+    'Captain',
+    'Catwoman',
+    'Fish',
+    'Hulk',
+    'Ninja',
+    'Black Cat',
+    'Volverine',
+    'Thor',
+    'Slayer',
+    'Vader',
+    'Slingo'
 ];
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
-
-    // Instead of forloop use Map method
-    // Code here
-
+    let detailedPlayers = players.map((player, index) => {
+        return {
+            name: player,
+            strength: getRandomStrength(),
+            image: `images/super-${index + 1}.png`,
+            type: index % 2 === 0 ? 'hero' : 'villain',
+            id: index + 1
+        };
+    });
     return detailedPlayers;
 }
 
@@ -38,12 +42,10 @@ const getRandomStrength = () => {
 
 // Build player template
 const buildPlayers = (players, type) => {
-    let fragment = '';
-
-    // Instead of using for loop
-    // Use chaining of Array methods - filter, map and join
-    // Type your code here
-
+    let fragment = players
+        .filter(player => player.type === type)
+        .map(player => view(player))
+        .join('');
     return fragment;
 }
 
@@ -53,6 +55,21 @@ const viewPlayers = (players) => {
     document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
 }
 
+const view = (playerObj) => {
+    let player = document.createElement('div');
+    player.classList.add('player');
+    let image = document.createElement('img');
+    image.setAttribute('src', playerObj.image);
+    image.setAttribute('alt', '');
+    let name = document.createElement('div');
+    name.className = 'name';
+    name.textContent = playerObj.name;
+    let strength = document.createElement('div');
+    strength.textContent = playerObj.strength;
+    strength.className = 'strength';
+    player.append(image, name, strength);
+    return player.outerHTML;
+};
 
 window.onload = () => {
     viewPlayers(initPlayers(PLAYERS));
